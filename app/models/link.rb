660aa20eb1a170b825c11a3244fc924388f9cf61
Preethikasd <mspreethika@gmail.com>
2017-07-16 14:24:43 +0800

    send and receive shortened link by api

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,12 @@
 class Link < ApplicationRecord
-	belongs_to :user
+	has_many :user_links
+	has_many :users, :through => :user_links
+
+	def self.get_short
+		numbers = (1..9).to_a
+		alphabets = ('a'..'z').to_a
+		alphanumeric = numbers + alphabets
+   		short_url=(0..10).map{ alphanumeric.to_a[rand(alphanumeric.size)] }.join
+	end
+
 end
